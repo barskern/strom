2bbe62935d9ad4476433de5352785421ef061a3c
Ole Martin Ruud <dev@barskern.no>
2023-01-24 05:38:08 +0100

    Updates to error handling

--- strom/fetch_prices.py
+++ strom/fetch_prices.py
@@ -34,26 +34,33 @@ def get_strom_timeseries(
     from_time: Optional[Date] = None,
     to_time: Optional[Date] = None,
 ):
-    start_date = from_time if from_time else pendulum.now('utc').date()
-    end_date = to_time if to_time else pendulum.now('utc').date()
+    start_date = from_time if from_time else pendulum.now("utc").date()
+    end_date = to_time if to_time else pendulum.now("utc").date()
 
     datas = []
-    date_range = pendulum.period(start_date, end_date).range('days')
+    date_range = pendulum.period(start_date, end_date).range("days")
     for date in date_range:
-        url = BASE_URL.format(year=date.year, month=date.month, day=date.day, price_area=price_area)
+        url = BASE_URL.format(
+            year=date.year, month=date.month, day=date.day, price_area=price_area
+        )
         try:
             res = session.get(str(url), json=True)
+            if res.status_code == 404:
+                logger.warning(f"Data for '{date.to_date_string()}' is not ready yet")
+
             data = res.json()
             datas.extend(data)
         except Exception as e:
-            logger.error(f"Unable to download data for day '{date}': {e}")
+            logger.error(
+                f"Unable to download data for day '{date.to_date_string()}': {e}"
+            )
 
     samples = []
     for data in datas:
         sample = []
 
-        sample.append(1000 * pendulum.parse(data['time_start']).int_timestamp)
-        sample.append(data['NOK_per_kWh'])
+        sample.append(1000 * pendulum.parse(data["time_start"]).int_timestamp)
+        sample.append(data["NOK_per_kWh"])
 
         samples.append(sample)
 
@@ -100,7 +107,9 @@ def main():
         start_time = pendulum.parse(sys.argv[1])
         end_time = pendulum.parse(sys.argv[2])
     else:
-        logging.info("Did not get any timestamps, running from last metric (if exists) to now")
+        logging.info(
+            "Did not get any timestamps, running from last metric (if exists) to now"
+        )
 
         start_time = None
         end_time = pendulum.now(tz="UTC") + pendulum.duration(1)
@@ -108,22 +117,18 @@ def main():
     session = requests.session()
     session.auth = HTTPBasicAuth(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
 
-    metric_name = 'price_electricity'
+    metric_name = "price_electricity"
 
     try:
         start_time_ = (
-            start_time
-            if start_time
-            else get_last_timestamp_in_metric(metric_name)
+            start_time if start_time else get_last_timestamp_in_metric(metric_name)
         )
         if end_time < start_time_:
-            logging.warning(
-                "Have a future (?) value in promscale, something is off.."
-            )
+            logging.warning("Have a future (?) value in promscale, something is off..")
             return
 
         logging.info(
-            f"Fetching electricity prices from '{start_time_}' to '{end_time}'"
+            f"Fetching electricity prices from '{start_time_.to_date_string()}' to '{end_time.to_date_string()}'"
         )
         timeseries = get_strom_timeseries(
             session, metric_name, PRICE_AREA, start_time_, end_time
